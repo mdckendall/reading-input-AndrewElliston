d50b8cfd40c83b06521d22c0a173abdd37b6d0a1
AndrewElliston <130585462+AndrewElliston@users.noreply.github.com>
2023-06-21 23:20:55 +0000

    .

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+   
+    
     System.out.println("Enter a word:");
+    String word = keyboard.nextLine();
+
+    System.out.println(word.length());
     
     //Your code will return the length of the entered String
   }
